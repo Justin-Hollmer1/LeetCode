bdb686facba7a2230120cde27c837c3bd1b78e8c
Justin Hollmer <justin.hollmer@gmail.com>
2023-02-19 20:00:10 -0600

    finally got it done, that took a while

--- LeetCode_Relative_Sort_Array.js
+++ LeetCode_Relative_Sort_Array.js
@@ -5,19 +5,44 @@
  */
 var relativeSortArray = function(arr1, arr2) {
     let newArr = [];
-    let duplicateArr = arr1;
+    let duplicateArr = [...arr1];
     for (let i = 0; i < arr2.length; i++) {
         for (let o = 0; o < arr1.length; o++) {
             if (arr2[i] === arr1[o]) {
                 newArr.push(arr1[o]);
-                duplicateArr.splice(o, 1);
+                duplicateArr.splice(o, 1, "Test");
+                // console.log("GOOD: arr2[" + i + "] is: " + arr2[i] + " and arr1["+ o + "] is: " + arr1[o])
             }
+            else {
+                // console.log("BAD: arr2[" + i + "] is: " + arr2[i] + " and arr1["+ o + "] is: " + arr1[o])
+            }
+            // console.log("arr1 is: " + arr1);
+            // }
+            // console.log("DuplicateArr is: " + duplicateArr);
+            // console.log("The newArr is: " + newArr);
         }
+        // console.log("New array is: " + newArr);
+        // console.log("Dupicate array is: " + duplicateArr);
     }
-    console.log("New array is: " + newArr);
-    console.log("Dupicate array is: " + duplicateArr);
-    console.log("Returning: " + newArr.concat(duplicateArr.sort()));
-    return newArr.concat(duplicateArr.sort());
-};
+    // Now to remove all of the 0 values from the duplicate array.
+    // duplicateArr = duplicateArr.sort();
+    // console.log("Before the loop, duplicate array is: " + duplicateArr)
+    // while (isNaN(duplicateArr[0])) {
+    //     duplicateArr.shift();
+    // }
 
-relativeSortArray([2,3,1,3,2,4,6,7,9,2,19],[ 2,1,4,3,9,6]);
+    for (let i = 0; i < duplicateArr.length; i++) {
+        if (isNaN(duplicateArr[i])) {
+            // console.log("Hit")
+            duplicateArr.splice(i, 1);
+            i--;
+        }
+        // console.log("New duplicate array is: " + duplicateArr);
+    }
+
+
+
+    // console.log("Duplicate array is: " + duplicateArr.sort(function(a, b){return a - b}));
+    // console.log("Returning: " + newArr.concat(duplicateArr.sort(function(a, b){return a - b})));
+    return newArr.concat(duplicateArr.sort(function(a, b){return a - b}));
+};
